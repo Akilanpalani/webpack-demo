880030ed03f34c639b8aacda1d4cdf5b7228aa61
Akilanpalani <akilanpalani2001@gmail.com>
2024-09-05 11:34:05 +0530

    feat: :sparkles: Added Webpack development process

--- webpack.config.js
+++ webpack.config.js
@@ -1,26 +1,33 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { optimize } = require('webpack');
 
 module.exports = {
-  mode: 'development',
+  mode: 'development', // Already avialable
   entry: {
     index: './src/index.js',
     print: './src/print.js',
   },
+  devtool: 'inline-source-map',
   plugins: [
     new HtmlWebpackPlugin({
-      title: 'Output Management',
+      title: 'Development',
     }),
   ],
   output: {
     filename: '[name].bundle.js',
     path: path.resolve(__dirname, 'dist'),
     clean: true,
+    publicPath: '/',
   },
   devServer: {
+    // dev server is already used in development initially
     static: {
       directory: path.join(__dirname, 'public'),
     },
     port: 3080,
+  },
+  optimization: {
+    runtimeChunk: 'single',
   },
 };
