c4bca795f3316ae7c14580a7093d023a44ab4564
Akilanpalani <akilanpalani2001@gmail.com>
2024-09-05 15:17:15 +0530

    fix: :sparkles: Webpack caching concept

--- src/index.js
+++ src/index.js
@@ -1,27 +1,24 @@
-import './styles/test.scss';
-import lowerCase from 'lodash';
-import csvData from './data.xml';
-import xmlData from './data.csv';
-
-import Icon from './images/avengers.jpg';
-
-// console.log(lowerCase('Hello, world!'));
+import _ from 'lodash';
+// import Print from './print';
 
 function component() {
   // craeate element for image
-  const element = document.querySelector('img');
-  element.src = Icon;
+  // const element = document.querySelector('img');
+  // element.src = Icon;
 
-  // add image to DOM
-  const myName = document.querySelector('.text');
-  const text = document.createTextNode('Hello, World!');
-  myName.appendChild(text);
+  // // add image to DOM
+  // const myName = document.querySelector('.text');
+  // const text = document.createTextNode('Hello, World!');
+  // myName.appendChild(text);
 
-  // Loading Data
-  console.log(csvData);
-  console.log(xmlData);
+  // return myName;
 
-  return myName;
+  const element = document.createElement('div');
+
+  element.innerHTML = _.join(['Hello', 'webpack'], ' ');
+  // element.onclick = Print.bind(null, 'Hello, World!');
+  return element;
 }
 
-component();
+// component();
+document.body.appendChild(component());
--- webpack.config.js
+++ webpack.config.js
@@ -10,36 +10,13 @@ module.exports = {
   entry: './src/index.js',
   output: {
     path: path.resolve(__dirname, 'dist'),
-    filename: 'bundle.js',
-  },
-  module: {
-    rules: [
-      {
-        test: /\.(js|jsx)$/i,
-        exclude: /node_modules/,
-        loader: 'babel-loader',
-      },
-      {
-        test: /\.scss$/,
-        use: ['style-loader', 'css-loader', 'sass-loader'],
-      },
-      {
-        test: /\.(eot|svg|ttf|woff|woff2|png|jpg|gif)$/i,
-        type: 'asset/resource',
-      },
-      {
-        test: /\.xml$/i,
-        use: ['xml-loader'],
-      },
-      {
-        test: /\.csv$/i,
-        use: ['csv-loader'],
-      },
-    ],
+    filename: '[name].[contenthash].js',
+    clean: true,
   },
   plugins: [
     new HtmlWebpackPlugin({
-      template: './public/index.html',
+      // template: './public/index.html',
+      title: 'Caching',
     }),
   ],
   devServer: {
@@ -49,10 +26,16 @@ module.exports = {
     port: 3080,
   },
   optimization: {
-    minimize: true,
-    minimizer: [
-      // new TerserPlugin(), //minimize javascript
-      new CssMinimizerPlugin(), //minimize css
-    ],
+    moduleIds: 'deterministic',
+    runtimeChunk: 'single',
+    splitChunks: {
+      cacheGroups: {
+        vendor: {
+          test: /[\\/]node_modules[\\/]/,
+          name: 'vendors',
+          chunks: 'all',
+        },
+      },
+    },
   },
 };
